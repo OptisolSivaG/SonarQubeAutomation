20b239993b79416aed5f0bbd85d0949191c47e23
SivaGurusamy <sivagurusamy@adt.com>
2024-09-05 16:46:11 +0530

    attempting to run actions

--- app/src/main/java/com/example/sonarqubeautomation/MainActivity.kt
+++ app/src/main/java/com/example/sonarqubeautomation/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SonarQubeAutomationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hi Siva",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
